205b9bd5f298c051c9c9d1328131b0e4dc15d54d
sasasak <geun02young@naver.com>
2026-01-07 11:47:14 +0900

    학습 : Falsy 로 평가되는 값 : 0, '', false, null, undefined, NaN

--- 자바스크립트/01-변수-데이터-타입/05-타입-변환/실습.js
+++ 자바스크립트/01-변수-데이터-타입/05-타입-변환/실습.js
@@ -285,17 +285,24 @@ console.log(Number.isNaN({}))
 // * 나머지 값은 모두 Truthy
 // --------------------------------------------------------------------------
 
+
 // 출력 결과: 0 → false
+console.log(Boolean(0))
 
 // 출력 결과: '' → false
+console.log(Boolean(''))
 
 // 출력 결과: false → false
+console.log(false)
 
 // 출력 결과: null → false
+console.log(!!null)
 
 // 출력 결과: undefined → false
+console.log(!!undefined)
 
 // 출력 결과: NaN → false
+console.log(!!NaN)
 
 
 // --------------------------------------------------------------------------
