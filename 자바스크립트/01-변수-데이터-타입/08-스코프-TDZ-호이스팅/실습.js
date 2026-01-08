dc081b9bbb58c899ebe880548bc3594fc14f2a92
sasasak <geun02young@naver.com>
2026-01-08 10:27:48 +0900

    학습 : 스코프 체이닝
    - outer 함수(지역)
    - inner 함수(중첩된 지역)

--- 자바스크립트/01-변수-데이터-타입/08-스코프-TDZ-호이스팅/실습.js
+++ 자바스크립트/01-변수-데이터-타입/08-스코프-TDZ-호이스팅/실습.js
@@ -99,8 +99,15 @@ const garden = '마당의 꽃'
 // console.log(blockVariable) // blockVariable is not defined - 블록 외부에서는 사용 불가 
 
 // 함수 myHouse 선언
-// - 지역 변수 desk 선언 ('책상 위의 노트북')
-// - 전역 변수 garden, 지역 변수 desk 콘솔에 출력
+function myHouse(){
+  // - 지역 변수 desk 선언 ('책상 위의 노트북')
+  const desk = '책상 위의 노트북'
+  // - 전역 변수 garden, 지역 변수 desk 콘솔에 출력
+  console.log('garden : ', garden)
+  console.log('desk : ' , desk)
+}
+
+myHouse()
 
 // 전역에서 garden, desk 변수 값 콘솔에 출력
 // - 함수 내부(지역)에서는 외부(전역) 변수에 접근할 수 있습니다.
@@ -116,13 +123,33 @@ const garden = '마당의 꽃'
 // --------------------------------------------------------------------------
 
 // 함수 outer 선언
-// - 지역 변수 livingRoom 선언 ('거실의 소파')
-// - 내부 함수 inner 선언
-//   - 지역 변수 myRoom 선언 ('내 방의 침대')
-//   - 전역 변수 garden, 상위 함수 변수 livingRoom, 지역 변수 myRoom 출력
-// - 내부 함수 inner 호출
+const outer = function() {
+  // - 지역 변수 livingRoom 선언 ('거실의 소파')
+  const livingRoom = '거실의 소파'
 
+  // - 내부 함수 inner 선언
+  const inner = function(){
+    //   - 지역 변수 myRoom 선언 ('내 방의 침대')
+    const myRoom = '내 방의 침대'
+    //   - 전역 변수 garden, 상위 함수 변수 livingRoom, 지역 변수 myRoom 출력
+    console.log(garden)
+    console.log(livingRoom)
+    console.log(myRoom)
+  }
+  // - 내부 함수 inner 호출
+  inner()
+//   - 전역 변수 garden, 상위 함수 변수 livingRoom, 지역 변수 myRoom 출력
+    console.log(garden)
+    console.log(livingRoom)
+    // console.log(myRoom)
+}
 // 함수 outer 호출
+outer()
+//   - 전역 변수 garden, 상위 함수 변수 livingRoom, 지역 변수 myRoom 출력
+    console.log(garden)
+    // console.log(livingRoom)
+    // console.log(myRoom)
+
 
 // 설명: 변수를 찾을 때 현재 범위에 없으면, 점점 더 바깥 범위로 나가며 찾습니다.
 // 출력 결과:
