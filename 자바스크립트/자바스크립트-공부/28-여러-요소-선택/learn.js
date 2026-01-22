d547ad459d5a6000357e25b31714ad754b6392e3
sasasak <geun02young@naver.com>
2026-01-22 14:31:55 +0900

    정리: 라이브 콜렉션이 생각만큼 유용하지 않은 이유 (예시)
    풀이: 여러 요소 선택 실습

--- 자바스크립트/자바스크립트-공부/28-여러-요소-선택/learn.js
+++ 자바스크립트/자바스크립트-공부/28-여러-요소-선택/learn.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 // --------------------------------------------------------------------------
 // 실습: 다중 요소 선택과 반복 (querySelectorAll vs getElementsBy...)
 // --------------------------------------------------------------------------
@@ -14,7 +15,6 @@ const sectionList = document.querySelectorAll('section')
   const sectionCount = sectionList.length
 
   while (i < sectionCount) {
-    // eslint-disable-next-line no-unused-vars
     const sectionItem = sectionList.item(i)
     // console.log(sectionItem)
     i = i + 1
@@ -28,7 +28,6 @@ const sectionList = document.querySelectorAll('section')
   let i = sectionList.length // 3
   
   while (--i >= 0) {
-    // eslint-disable-next-line no-unused-vars
     const sectionItem = sectionList.item(i)
     // console.log(sectionItem)
     // i -= 1
@@ -39,14 +38,12 @@ const sectionList = document.querySelectorAll('section')
 {
   // 순방향(정순)
   for (let i = 0, l = sectionList.length; i < l; ++i) {
-    // eslint-disable-next-line no-unused-vars
     const sectionItem = sectionList.item(i)
     // console.log(sectionItem)
   }
 
   // 역방향(역순)
   for (let i = sectionList.length - 1; i >= 0; --i) {
-    // eslint-disable-next-line no-unused-vars
     const sectionItem = sectionList[i]
     // console.log(sectionItem)
   }
@@ -55,7 +52,6 @@ const sectionList = document.querySelectorAll('section')
 // for...of 문
 {
   // NodeList [section, section, section]
-  // eslint-disable-next-line no-unused-vars
   for (const sectionItem of sectionList) {
     // console.log(sectionItem)
   }
@@ -63,7 +59,6 @@ const sectionList = document.querySelectorAll('section')
 
 // forEach 메서드
 {
-  // eslint-disable-next-line no-unused-vars
   sectionList.forEach((section, key) => {
     // console.log(key, section)
   })
@@ -79,7 +74,6 @@ const headerChildren = document.querySelectorAll('.prose header > *')
 {
   // for 문
   for (let i = 0, l = headerChildren.length; i<l; ++i) {
-    // eslint-disable-next-line no-unused-vars
     const childElement = headerChildren.item(i)
     // console.log(i, childElement)
     // childElement.classList.add('color--primary')
@@ -88,7 +82,6 @@ const headerChildren = document.querySelectorAll('.prose header > *')
 
 {
   // for...of 문
-  // eslint-disable-next-line no-unused-vars
   for (const childElement of headerChildren) {
     // console.log(childElement)
     // childElement.classList.add('color--primary')
@@ -97,7 +90,6 @@ const headerChildren = document.querySelectorAll('.prose header > *')
 
 {
   // forEach 메서드
-  // eslint-disable-next-line no-unused-vars
   headerChildren.forEach((childElement) => {
     // console.log(childElement)
     // childElement.classList.add('color--primary')
@@ -109,9 +101,9 @@ const headerChildren = document.querySelectorAll('.prose header > *')
   // 유사 배열 객체 → 배열 객체로 생성해 반환
 
   const articleChidlren = document.querySelectorAll('main > article > *')
-  console.log(typeof articleChidlren) // NodeList
+  // console.log(typeof articleChidlren) // NodeList
   const articleChildrenArray = Array.from(articleChidlren) // Array
-  console.log(articleChildrenArray)
+  // console.log(articleChildrenArray)
 
 }
 
@@ -119,11 +111,11 @@ const headerChildren = document.querySelectorAll('.prose header > *')
 // [실습] querySelectorAll을 이용한 다중 선택
 // 1. '.life-tips' 내부의 모든 'p' 요소를 선택하여 'paragraphs' 변수에 할당하세요.
 // 2. 수집된 NodeList의 길이를 콘솔에 출력하세요.
-console.group('1. 요소 일괄 선택')
+console.groupCollapsed('1. 요소 일괄 선택')
 
 // 여기에 코드를 작성하세요.
 const paragraphs = document.querySelectorAll('.life-tips p')
-console.log(paragraphs.length)
+// console.log(paragraphs.length)
 
 console.groupEnd()
 
@@ -131,7 +123,7 @@ console.groupEnd()
 // [실습] 다양한 반복문을 활용한 스타일 적용
 // 1. for...of 문을 사용하여 모든 단락에 'color-primary' 클래스를 추가하세요.
 // 2. forEach 메서드를 사용하여 모든 단락의 배경색을 변경해 보세요.
-console.group('2. NodeList 루프 수행')
+console.groupCollapsed('2. NodeList 루프 수행')
 
 // 여기에 코드를 작성하세요.
 
@@ -141,7 +133,7 @@ console.groupEnd()
 // [실습] NodeList를 진짜 배열로 변환하기
 // 1. Array.from()을 사용하여 'paragraphs'를 'paragraphArray'로 변환하세요.
 // 2. Array.isArray()를 통해 변환 여부를 확인하고 콘솔에 출력하세요.
-console.group('3. 배열로 변환')
+console.groupCollapsed('3. 배열로 변환')
 
 // 여기에 코드를 작성하세요.
 const allParagraph = document.querySelectorAll('main p')
@@ -162,7 +154,7 @@ console.groupEnd()
 // 1. getElementsByTagName을 사용하여 라이브 컬렉션을 'liveCol'에 담으세요.
 // 2. querySelectorAll을 사용하여 정적 컬렉션을 'staticCol'에 담으세요.
 // 3. 부모 요소에 innerHTML을 사용하여 새로운 <p>를 추가한 후, 두 컬렉션의 length 차이를 확인하세요.
-console.group('4. Live vs Static 컬렉션')
+console.groupCollapsed('4. Live vs Static 컬렉션')
 
 // 여기에 코드를 작성하세요.
 
@@ -171,14 +163,19 @@ console.groupEnd()
 
 // --------------------------------------------------------------------------
 // 여러 요소를 선택하는 대체 방법
-// * getElementsByTagName()
-// * getElementsByClassName()
+// * getElementsByTagName()    → HTMLCollection
+// * getElementsByClassName()  → HTMLCollection
 // --------------------------------------------------------------------------
 
 // 문서의 모든 섹션 요소 찾아보자.
 const allSectionElements = document.getElementsByTagName('section')
-
 // console.log(allSectionElements) // HTMLCollection
+
+// 클래스 속성 이름으로 문서에서 요소들을 수집해보자.
+const allRegionByClassName = document.getElementsByClassName('region')
+// console.log(allRegionByClassName) // HTMLCollection
+
+
 
 // TypeError: allSectionElements.forEach is not a function
 // 반복하고 싶은데??? 어떻게 해야 할까?
@@ -192,14 +189,12 @@ const allSectionElements = document.getElementsByTagName('section')
 // - ✅ for (index가 필요해!)
 {
   for (let i = 0, l = allSectionElements.length; i < l; ++i) {
-    // eslint-disable-next-line no-unused-vars
     const element = allSectionElements[i] // 또는 .item(i) 메서드 활용
     // console.log(element)
   }
 }
 // - ✅ for...of
 {
-  // eslint-disable-next-line no-unused-vars
   for(const element of allSectionElements) {
     // console.log(element)
   }
@@ -208,28 +203,122 @@ const allSectionElements = document.getElementsByTagName('section')
 // - ⚠️ for...in (객체 순환용)
 // - ❌ forEach (지원하지 않음) : 아... 이 방법 쓰고 싶은데...
 //   🌻 아하! Array.from() 메서드가 있었지!! 배열로 바꿔서 해보자!
-console.log(Array.isArray(allSectionElements)) // false
-console.log(Array.isArray(Array.from(allSectionElements))) // true
+// console.log(Array.isArray(allSectionElements)) // false
+
+const allSectionElementsArray = Array.from(allSectionElements)
+// console.log(Array.isArray(allSectionElementsArray)) // true
+
+allSectionElementsArray.forEach((el) => {
+  // console.log(el)
+})
 
 
+// --------------------------------------------------------------------------
+// NodeList vs HTMLCollection
+// --------------------------------------------------------------------------
+// NodeList (Static Collection)     : querySelector, querySelectorAll
+// HTMLCollection (Live Collection) : getElementsByTagName, getElementsByClassName
+// --------------------------------------------------------------------------
+
+console.group('NodeList vs HTMLCollection')
+
+const lifeTips = document.querySelector('.life-tips')
+console.log(lifeTips) // HTMLElement (Node)
+
+// Static Collection (NodeList)
+const staticCollection = lifeTips.querySelectorAll('p')
+console.log(staticCollection)
+
+// Live Collection (HTMLCollection)
+const liveCollection = lifeTips.getElementsByTagName('p')
+console.log(liveCollection)
+
+// 현재는 스태틱/라이브 콜렉션 모두 동일해 보이지만...
+// 동적으로 DOM 조작(Manipulation) -> 화면 변경(update view)
+
+// 비동기 스케쥴링(시간 차를 두고 일을 한다.)
+// 1초 뒤에 일을 할께요~
+// setTimeout(() => {
+//   lifeTips.innerHTML += '<p>씻지 않은 채소는 그대로 보관하시는 것이 좋습니다.</p>'
+
+//   // static collection의 수집된 개수
+//   console.log('static collection\n', staticCollection)
+//   // live collection의 수집된 개수
+//   console.log('live collection\n', liveCollection)
+
+// }, 3000)
 
 
+// 라이브 콜렉션
+// - getElementsByClassName
+// - getElementsByTagName
+const liveC = lifeTips.querySelector('.live-collection') // HTMLULElement <ul>
+const liveCItems = liveC.getElementsByTagName('li') // HTMLCollection [li, li, li]
 
+for(const item of liveCItems) {
+  item.addEventListener('click', () => {
+    console.log('click:', item.textContent)
+  })
+}
 
+// 비동기(async) 스케쥴링
+// 3초 뒤에 DOM 조작 (동적 변경)
+setTimeout(manipulateDOM, 3000)
 
-
-
+console.groupEnd()
 
 
 // [미션 5] 라이브 컬렉션과 이벤트 연결의 한계
 // 1. '.live-collection li' 요소들을 getElementsByTagName으로 수집하세요.
 // 2. 루프를 돌며 클릭 시 textContent를 출력하는 이벤트를 연결하세요.
 // 3. 새로운 <li>를 추가한 후, 새 요소에서도 클릭 이벤트가 작동하는지 확인하세요. (작동하지 않음이 정상)
-console.group('5. 이벤트 위임의 필요성 체감')
+console.groupCollapsed('5. 이벤트 위임의 필요성 체감')
 
 // 여기에 코드를 작성하세요.
 
 console.groupEnd()
+
+
+
+// --------------------------------------------------------------------------
+// 실습 
+// --------------------------------------------------------------------------
+
+// 1. 좋은 사람(Good Guy)을 모두 선택하세요.
+const goodGuys = document.querySelectorAll('[data-type="good-guy"]')
+// console.log(goodGuys)
+
+// 2. 좋은 사람(Good Guy) 모두에게 `excellent` 클래스 이름을 추가하세요.
+// goodGuys.forEach((guy) => guy.classList.add('excellent'))
+for (const guy of goodGuys) {
+  guy.classList.add('excellent')
+}
+
+// 3. 빌런(Villain)을 모두 선택하세요.
+const villains = document.querySelectorAll('[data-type="villain"]')
+// console.log(villains)
+
+// 4. 빌런 모두에게 `naughty` 클래스 이름을 추가하세요.
+// for (const villain of villains) {
+//   villain.classList.add('naughty')
+// }
+villains.forEach((villain) => villain.classList.add('naughty'))
+
+// 5. 모든 캐릭터(Character)를 선택하세요.
+const characters = document.querySelectorAll('.character')
+// console.log(characters)
+
+// 6. 모든 캐릭터에 `star-wars` 클래스 이름을 추가하세요.
+// characters.forEach((character) => character.classList.add('star-wars'))
+for (const character of characters) {
+  character.classList.add('star-wars')
+}
+
+
+
+
+
+
 
 
 // --------------------------------------------------------------------------
@@ -241,3 +330,12 @@ console.groupEnd()
 // 4. 새로운 요소가 추가되어도 이미 연결된 이벤트는 자동으로 전파되지 않으므로 '이벤트 위임'이 유리합니다.
 // 5. Array.from(nodeList)를 사용하면 NodeList를 완전한 배열로 변환하여 map, filter 등을 쓸 수 있습니다.
 // --------------------------------------------------------------------------
+
+
+
+
+
+function manipulateDOM() {
+  liveC.insertAdjacentHTML('beforeend', '<li>나중에 추가된 요소 (클릭 시 반응 없음)</li>')
+  console.log('새 요소가 추가되었습니다. 하지만 클릭 이벤트는 작동하지 않습니다.')
+}
