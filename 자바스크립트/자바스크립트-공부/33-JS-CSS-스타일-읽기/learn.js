b8053630bf115133149567456aa311bf8704cd5c
sasasak <geun02young@naver.com>
2026-01-26 09:30:57 +0900

    학습: JS로 CSS 스타일 읽기 - getComputedStyle - element.style

--- 자바스크립트/자바스크립트-공부/33-JS-CSS-스타일-읽기/learn.js
+++ 자바스크립트/자바스크립트-공부/33-JS-CSS-스타일-읽기/learn.js
@@ -10,6 +10,12 @@ console.groupCollapsed('1. 인라인 스타일 읽기 (style 속성)')
 
 // 이곳에 코드를 작성하세요.
 
+const prose = document.querySelector('.prose')
+const targetElement = prose.querySelector('section:first-of-type h2')
+// console.log(targetElement)
+// console.log(targetElement.style.fontSize)
+// console.log(targetElement.style.cssText)
+
 
 console.groupEnd()
 
@@ -22,6 +28,21 @@ console.groupEnd()
 console.groupCollapsed('2. 계산된 스타일 읽기 (getComputedStyle)')
 
 // 이곳에 코드를 작성하세요.
+
+const heading1 = prose.querySelector('h1')
+// console.log(heading1.style.cssText)
+const heading1ComputedStyle = getComputedStyle(heading1) // CSSStyleDeclaration
+// console.log(heading1ComputedStyle.getPropertyValue('font-size'))
+// console.log(heading1ComputedStyle.getPropertyValue('color'))
+// console.log(heading1ComputedStyle.getPropertyValue('letter-spacing'))
+
+const sizeUpButton = document.querySelector('.js-size-up-button')
+sizeUpButton.addEventListener('click', () => {
+  let currentFontSize = heading1ComputedStyle.getPropertyValue('font-size')
+  currentFontSize = parseInt(currentFontSize, 10)
+  const nextFontSize = currentFontSize + 8
+  heading1.style.setProperty('font-size', nextFontSize + 'px')
+})
 
 
 console.groupEnd()
