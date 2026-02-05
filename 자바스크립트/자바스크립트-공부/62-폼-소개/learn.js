454065f40c721dd91279af220fa6df3156057e64
sasasak <geun02young@naver.com>
2026-02-05 10:21:39 +0900

    학습: 폼(Form) 소개 - 클라이언트 제어 - 브라우저 기본 작동 방지 - 폼 전송 메서드 (form.submit()) - 폼 초기화 메서드 (form.reset())

--- 자바스크립트/자바스크립트-공부/62-폼-소개/learn.js
+++ 자바스크립트/자바스크립트-공부/62-폼-소개/learn.js
@@ -2,6 +2,32 @@
 // 실습: 폼 제출 제어 (Form Submission Control)
 // --------------------------------------------------------------------------
 
+const emailForm = document.querySelector('.send-email-form')
+
+emailForm.addEventListener('submit', (e) => {
+  const form = e.currentTarget // emailForm
+
+  // 브라우저 기본 작동(서버 프로그램으로 입력된 정보 전달, 페이지 이동) 방지
+  e.preventDefault()
+
+  // 폼을 통해 입력되 사용자의 정보를 제어
+  console.log('폼을 통해 입력되 사용자의 정보를 제어')
+
+  // 클라이언트 측에서 JavaScript를 사용해 
+  // 사용자 입력 정보가 유효한지 확인
+  const isAllValid = true
+
+  // 사용자 입력 정보를 서버에 전송 명령
+  if (isAllValid) {
+    // 폼 전송 (form.submit())
+    form.submit()
+  }
+
+  // 폼 초기화 (native method reset)
+  form.reset()
+
+  // 직접 초기화 (manual reset)
+})
 
 
 // --------------------------------------------------------------------------
