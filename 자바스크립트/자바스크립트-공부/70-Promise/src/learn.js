8db950f43e14a1e413f05c518c2681e3eab9d634
sasasak <geun02young@naver.com>
2026-02-06 15:16:08 +0900

    학습: Promise 객체 - 미결 상태 - 해결 상태 - 거절 상태

--- 자바스크립트/자바스크립트-공부/70-Promise/src/learn.js
+++ 자바스크립트/자바스크립트-공부/70-Promise/src/learn.js
@@ -2,6 +2,35 @@
 // 실습: 프로미스(Promise) 객체 생성 및 체이닝
 // --------------------------------------------------------------------------
 
+const TIMEOUT = 3 // 3s
+
+// Promise 생성자 함수를 사용해 인스턴스 생성
+const promiseInstance = new Promise((resolve, reject) => {
+  // console.log(typeof resolve) // 'function'
+  // console.log(typeof reject) // 'function'
+
+  // 비동기 작업 처리
+  // 비동기 스케쥴링 (3초 뒤 타임아웃)
+  setTimeout(() => {
+    // 아직은 모르는 미결(pending) 상태
+    // 둘 중 하나 성공(해결) 또는 실패(거절)
+    if (Math.random() >= 0.05) {
+      resolve()
+    } else {
+      reject()
+    }
+
+  }, TIMEOUT * 1000)
+})
+
+// Promise 객체 (생성해야 사용 가능)
+// Promise { then, catch, finally }
+// pending (미결, 보류, pending) 
+promiseInstance
+  // -> fulfilled (이행, 해결, fulfilled) .then(callback)
+  .then(() => console.log('약속이 이행되었어! 😊'))
+  // -> rejected (거부, 거절, rejected) .catch(callback)
+  .catch(() => console.warn('약속이... 거절되었어.. 😭'))
 
 
 // --------------------------------------------------------------------------
